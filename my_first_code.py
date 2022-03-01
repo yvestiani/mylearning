e8e998451452a7abe3753d842d88e82c0d6b3bf6
yvestiani <yvestiani@gmail.com>
2022-03-01 15:48:21 -0500

    commit new changes

--- my_first_code.py
+++ my_first_code.py
@@ -1,4 +1,9 @@
-print ("I love banana and mango")
+Useful Command in Python
 
 # Type() is use to display the datatype of the variable
+
 # round(number, decimal place) 
+
+# Reversing a string using the slicing is string[::-1] this will step into the string one time in reverse. 
+# To get a substring of a string you can use the START and STOP. START [2:] will start from the 3rd string all the way to the end. 
+# STOP [:2] will strat from the 3rd string all the way to the begining. 
